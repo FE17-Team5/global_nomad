7a1a28913322b8dc5576fa06d027014226a675ac
limheewoo <gmldn39@naver.com>
2025-09-24 23:03:14 +0900

    Init: 프로젝트 초기 세팅

--- src/main-layout.tsx
+++ src/main-layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/header";
+import Footer from "./components/Footer/footer";
 
 const MainLayout = () => {
   return (
@@ -8,6 +9,7 @@ const MainLayout = () => {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 };
